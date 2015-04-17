caeb0b960df3430ef8cc9bd61a216d704b302157
stefano <13201749@ucdconnect.ie>
2015-04-17 22:58:44 +0200

    queries

--- app/src/main/java/alpha_team/myvodafone_alpha_team/helper/JSONParser.java
+++ app/src/main/java/alpha_team/myvodafone_alpha_team/helper/JSONParser.java
@@ -24,7 +24,7 @@ public class JSONParser {
         if (service == 0) { //calls
 
             //String urlQuery = "http://"+URL+":2480/query/vf/sql/select RATED_FLAT_AMOUNT_EURO from rtxh where (START_D_T >= '" + dateStart + "' AND (START_D_T <= '" + dateEnd + "') AND (CALL_TYPE = 1) AND (SNCODE = 1)";
-            String urlQuery = "http://"+URL+":2480/query/vf/sql/select RATED_FLAT_AMOUNT_EURO from rtxh where (START_D_T >= '" + dateStart + "' AND (START_D_T <= '" + dateEnd + "') AND (CALL_TYPE = 1) AND (SNCODE = 1)";
+            String urlQuery = "http://" + URL + ":2480/query/vf/sql/select%20RATED_FLAT_AMOUNT_EURO%20from%20rtxh%20where%20(START_D_T%20>=%20'" + dateStart + "')%20AND%20(START_D_T%20<=%20'" + dateEnd + "')%20and%20(CALL_TYPE%20=%201)%20and%20(SNCODE%20=%201)";
 
             try {
                 s = HelperHttp.downloadUrl(urlQuery);
@@ -32,9 +32,20 @@ public class JSONParser {
                 System.err.println("errore");
             }
 
+            if (!s.isEmpty()) {
+                arr = (JSONArray) HelperHttp.stringToJsonArray(s);
 
+                for (int i = 0; i < arr.length(); i++) {
+                    try {
+                        sum += (arr.getJSONObject(i).getDouble("RATED_FLAT_AMOUNT_EURO"));
+                    } catch (Exception e) {
+
+                    }
+                }
+                return sum;
+            }
         } else if (service == 1) { //sms
-            String urlQuery = "http://"+URL+":2480/query/vf/sql/select%20RATED_FLAT_AMOUNT_EURO%20from%20rtxh%20where%20(START_D_T%20>=%20'" + dateStart + "')%20AND%20(START_D_T%20<=%20'" + dateEnd + "')%20and%20(CALL_TYPE%20=%201)%20and%20(SNCODE%20=%2014)";
+            String urlQuery = "http://" + URL + ":2480/query/vf/sql/select%20RATED_FLAT_AMOUNT_EURO%20from%20rtxh%20where%20(START_D_T%20>=%20'" + dateStart + "')%20AND%20(START_D_T%20<=%20'" + dateEnd + "')%20and%20(CALL_TYPE%20=%201)%20and%20(SNCODE%20=%2014)";
             try {
                 s = HelperHttp.downloadUrl(urlQuery);
             } catch (Exception e) {
@@ -55,21 +66,51 @@ public class JSONParser {
             }
 
         } else if (service == 2) { //data
-            String urlQuery = "http://"+URL+":2480/query/vf/sql/select RATED_FLAT_AMOUNT_EURO from rtxh where (START_D_T >= " + dateStart + "' AND (START_D_T <= '" + dateEnd + "') and (CALL_TYPE = 10) and (SNCODE = 508)";
+            //String urlQuery = "http://"+URL+":2480/query/vf/sql/select RATED_FLAT_AMOUNT_EURO from rtxh where (START_D_T >= " + dateStart + "' AND (START_D_T <= '" + dateEnd + "') and (CALL_TYPE = 10) and (SNCODE = 508)";
+            String urlQuery = "http://" + URL + ":2480/query/vf/sql/select%20RATED_FLAT_AMOUNT_EURO%20from%20rtxh%20where%20(START_D_T%20>=%20'" + dateStart + "')%20AND%20(START_D_T%20<=%20'" + dateEnd + "')%20and%20(CALL_TYPE%20=%2010)%20and%20(SNCODE%20=%20508)";
             try {
                 s = HelperHttp.downloadUrl(urlQuery);
             } catch (Exception e) {
                 System.err.println("errore");
             }
+
+            if (!s.isEmpty()) {
+                arr = (JSONArray) HelperHttp.stringToJsonArray(s);
+
+                for (int i = 0; i < arr.length(); i++) {
+                    try {
+                        sum += (arr.getJSONObject(i).getDouble("RATED_FLAT_AMOUNT_EURO"));
+                    } catch (Exception e) {
+
+                    }
+                }
+                return sum;
+            }
+
         } else if (service == 3) { //extra VAR
-            String urlQuery = "http://"+URL+":2480/query/vf/sql/select RATED_FLAT_AMOUNT_EURO from rtxh_ch where (START_D_T >= " + dateStart + "' AND (START_D_T <= '" + dateEnd + "')";
+            //String urlQuery = "http://"+URL+":2480/query/vf/sql/select RATED_FLAT_AMOUNT_EURO from rtxh_ch where (START_D_T >= " + dateStart + "' AND (START_D_T <= '" + dateEnd + "')";
+            String urlQuery = "http://" + URL + ":2480/query/vf/sql/select%20RATED_FLAT_AMOUNT_EURO%20from%20rtxh%20where%20(START_D_T%20>=%20'" + dateStart + "')%20AND%20(START_D_T%20<=%20'" + dateEnd + "')";
             try {
                 s = HelperHttp.downloadUrl(urlQuery);
             } catch (Exception e) {
                 System.err.println("errore");
             }
+
+            if (!s.isEmpty()) {
+                arr = (JSONArray) HelperHttp.stringToJsonArray(s);
+
+                for (int i = 0; i < arr.length(); i++) {
+                    try {
+                        sum += (arr.getJSONObject(i).getDouble("RATED_FLAT_AMOUNT_EURO"));
+                    } catch (Exception e) {
+
+                    }
+                }
+                return sum;
+            }
+
         }
 
-    return 0;
+        return 0;
     }
 }
